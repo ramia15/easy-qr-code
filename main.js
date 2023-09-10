ec7ac5886d9f40f3a567241a33aacf5d5757eddf
Rami <53275043+ramia15@users.noreply.github.com>
2023-09-09 21:37:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,6 @@
 let originalUrl = '';
 let urlShortened = false; // Add a flag to track if URL is shortened
 
-// Use the environment variable set in the .env file
-const serverUrl = '/api/shorten'; // Update this to the correct endpoint
-
 // Add an event listener to the "Shorten URL" button
 document.getElementById('shortenButton').addEventListener('click', async () => {
     const urlInput = document.getElementById('qrUrl');
@@ -24,7 +21,7 @@ document.getElementById('shortenButton').addEventListener('click', async () => {
 
             // Add a custom class to visually disable the button
             shortenButton.classList.add('custom-disabled');
-            const response = await fetch(serverUrl, { // Use the correct serverless function URL
+            const response = await fetch('/api/shorten', { // Use the relative path to your serverless function
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
